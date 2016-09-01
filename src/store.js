aca04feaa110ca45e231999581a3d1498a7062cf
Viktor Osychniuk <fderty3@gmail.com>
2016-09-01 14:23:21 +0300

    clean up comments

--- src/store.js
+++ src/store.js
@@ -84,77 +84,6 @@ class Store {
       throw new TypeError(`${transformerOrSaga} must be either a transformer a saga.`)
     }
   }
-  /*select(query) {
-    const select = query => this.select(query);
-    const selectParent = query => this.parent.select(query);
-    const dataOrSelector = this.opts.select(
-      this.state,
-      query,
-      select,
-      selectParent);
-    if (isFunction(dataOrSelector)) {
-      const selector = dataOrSelector;
-      return () => selector(this.state);
-    }
-    else {
-      const selector = st => this.opts.select(st, query);
-      return () => selector(this.state);
-    }
-  }
-  dispatch(msg) {
-    const stateOrSaga = this.opts.dispatch(
-      this.state,
-      msg,
-      dispatch,
-      dispatchParent);
-    if (isPromise(stateOrSaga)) {
-      const saga = stateOrSaga;
-      return saga.then(true);
-    }
-    else {
-      const state = stateOrSaga;
-      this.setState(state);
-    }
-  }*/
-  /*dispatch(msg) {
-    const transformerOrSaga = this.opts.dispatch(
-      msg,
-      dispatch,
-      dispatchParent);
-    if (isFunction(transformerOrSaga)) {
-      const transformer = transformerOrSaga;
-      this.setState(transformer(this.state));
-      return promise.resolve(true);
-    }
-    else if (isPromise(transformerOrSaga)) {
-      const saga = transformerOrSaga;
-      return saga.then(true);
-    }
-    else {
-      throw new TypeError(`No saga or transformer found for ${msg}`);
-    }
-  }*/
-  /*dispatch(msg) {
-    const dispatch = msg => this.dispatch(msg);
-    const dispatchParent = msg => this.parent.dispatch(msg);
-    const dispatching = this.opts.dispatch(
-      msg,
-      dispatch,
-      dispatchParent);
-    if (isUndefined(dispatching)) {
-      return promise.resolve(true);
-    }
-    else if (isPromise(dispatching)) {
-
-    }
-    else {
-      throw new TypeError(`No dispatcher found for ${msg}`);
-    }
-  }*/
-  /*transform(msg) {
-    const state = this.opts.transform(this.state, msg);
-    this.setState(state);
-  }*/
   getState() {
     return this.state;
   }
@@ -165,41 +94,6 @@ class Store {
     }
     return this;
   }
-  /*state(state) {
-    if (isUndefined(state)) {
-      return this._state;
-    }
-    else {
-      let newState = this._state;
-      if (isFunction(state)) {
-        newState = state(newState);
-      }
-      else {
-        newState = state;
-      }
-      if (this._state !== newState) {
-        this._state = newState;
-        this.scheduleNotify();
-      }
-    }
-  }*/
-  /*subscribeParent(callback) {
-    return this.parent.subscribe(callback);
-  }
-  subscribeSelf(callback) {
-    this.subscribers.add(callback);
-    return () => {
-      this.subscribers.delete(callback);
-    };
-  }
-  subscribe(callback) {
-    const unsubscribeParent = this.subscribeParent(callback);
-    const unsubscribeSelf = this.subscribeSelf(callback);
-    return () => {
-      unsubscribeParent();
-      unsubscribeSelf();
-    };
-  }*/
   subscribe(callback) {
     this.subscribers.add(callback);
     return () => {
@@ -215,16 +109,6 @@ class Store {
     return store;
   }
 }
-
-/*class NS {
-  constructor(parent) {
-
-  }
-  select(query) {}
-  subscribe(callback) {
-    return this.parent.subscribe(callback);
-  }
-}*/
 
 const store = opts =>
   new Store(opts);
